eb7d293c7c3798bbd05b643b0009fb1f7a7f9d5d
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-20 17:30:35 +0530

    see more btn added

--- src/components/Countrys/Countrys.jsx
+++ src/components/Countrys/Countrys.jsx
@@ -3,19 +3,25 @@ import Country from "./country";
 import { useEffect } from "react";
 
 const Countrys = () => {
+      const [sliceData, setSliceData] = useState(20)
       const [countrys, setCountrys] = useState([])
       useEffect(() => {
             fetch('https://restcountries.com/v3.1/all')
                   .then(res => res.json())
-                  .then(data => setCountrys(data))
-      }, [])
+                  .then(data => setCountrys(data.slice(0, sliceData)))
+      }, [sliceData])
+
+      const handelSeeMore = () => {
+            setSliceData(sliceData + 20)
+      }
 
       return (
             <div className=" container mx-auto mt-7">
 
-                  <div className=" grid grid-cols-4 justify-between gap-4">
+                  <div className=" w-11/12 mx-auto md:w-full grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 justify-between gap-4 ">
                         {countrys.map(country => <Country key={country.cca3} country={country}></Country>)}
                   </div>
+                  <div className=" w-full flex  justify-center"><button className={` btn text-center my-7 border-cyan-400 text-cyan-400 ${countrys.length == 250 && 'hidden'}`} onClick={handelSeeMore}>See more...</button></div>
             </div>
       );
 };
--- src/components/Countrys/country.jsx
+++ src/components/Countrys/country.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 
 const Country = ({ country }) => {
       const { name, flags, currencies, timezones } = country
       const currenciesName = Object.values(currencies || {})
       console.log(currenciesName);
+      const [isVisited, setVisited] = useState(false)
+      const handelVisited = () => {
 
-
+            setVisited(!isVisited)
+      }
       return (
             <div className=" flex flex-col gap-2 border border-gray-400 shadow-md shadow-black p-4 rounded-lg">
                   <img className=" w-32 h-20" src={flags.png} alt="" />
@@ -12,6 +16,7 @@ const Country = ({ country }) => {
                   <p>Courrencies :{currenciesName?.[0]?.name} </p>
                   <p>Symbol :  {currenciesName?.[0]?.symbol} </p>
                   <p>Time zones :  {timezones[0]} </p>
+                  <button onClick={handelVisited} className={` btn `}>{isVisited ? "Visited" : "visit"}</button>
             </div>
       );
 };
